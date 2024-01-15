d7e55f5445c8a4f14b64715886e27830e7ba200e
Adriana Torres <adrianaamtorres@gmail.com>
2024-01-15 16:15:56 -0600

    trips request/response

---
+++ Sources/AdriciaSharedDTO/Trips/TripResponseDTO.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adriana Torres on 1/15/24.
+//
+
+import Foundation
+
+public struct TripResponseDTO: Codable {
+    
+    public let id: UUID
+    public let destination: String
+    public let departureDate: String
+    public let departureTime: String
+    public let returnDate: String
+    public let returnTime: String
+    public let isFlying: Bool
+    
+    public init(
+        id: UUID,
+        destination: String,
+        departureDate: String,
+        departureTime: String,
+        returnDate: String,
+        returnTime: String,
+        isFlying: Bool
+    ) {
+        self.id = id
+        self.destination = destination
+        self.departureDate = departureDate
+        self.departureTime = departureTime
+        self.returnDate = returnDate
+        self.returnTime = returnTime
+        self.isFlying = isFlying
+    }
+}
