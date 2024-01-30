f49bce18d2e33145cc37edc6038446d917fa75d4
Adriana Torres <adrianaamtorres@gmail.com>
2024-01-30 11:04:34 -0600

    updating-plans

--- Sources/AdriciaSharedDTO/Plans/PlanResponseDTO.swift
+++ Sources/AdriciaSharedDTO/Plans/PlanResponseDTO.swift
@@ -13,16 +13,25 @@ public struct PlanResponseDTO: Codable {
     public let event: String
     public let eventDate: String
     public let eventTime: String
+    public let address: String
+    public let image: String
+    public let isTBD: Bool
     
     public init(
         id: UUID,
         event: String,
         eventDate: String,
-        eventTime: String
+        eventTime: String,
+        address: String,
+        image: String,
+        isTBD: Bool
     ) {
         self.id = id
         self.event = event
         self.eventDate = eventDate
         self.eventTime = eventTime
+        self.address = address
+        self.image = image
+        self.isTBD = isTBD
     }
 }
